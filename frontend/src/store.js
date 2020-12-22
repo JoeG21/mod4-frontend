39f9cc23426925ac6c297667f299f0fbdda0c59d
JoeG21 <jcggomez@live.com>
2020-12-21 22:20:22 -0600

    store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,20 @@
-// export default createStore(
-//     reducer,
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// )
-
 import { createStore } from 'redux';
 import noteReducer from './reducers/noteReducer'
 
-const store = createStore(
-    noteReducer,
+export default createStore(
+    reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-export default store;
+// import { createStore } from 'redux';
+// import noteReducer from './reducers/noteReducer'
+
+// const store = createStore(
+//     noteReducer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
+
+// export default store;
+
+
+
